e9c92004766ce5414adbc3591b0f58771a31dc76
Christian Landgren <christian@landgren.nu>
2024-02-01 22:41:12 +0100

    fix: add chroma token

--- src/config/chromadb.ts
+++ src/config/chromadb.ts
@@ -1,3 +1,9 @@
 export default {
   path: process.env.CHROMA_HOST || 'http://127.0.0.1:8000',
+  auth: process.env.CHROMA_TOKEN
+    ? {
+        provider: 'token',
+        credentials: process.env.CHROMA_TOKEN || '',
+      }
+    : undefined,
 }
