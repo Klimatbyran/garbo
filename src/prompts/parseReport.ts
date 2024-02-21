5fd022b0ae392caa105b0f12e3742900ea2c1f8a
Elvira <elvbom@users.noreply.github.com>
2024-02-21 21:50:50 +0100

    minor prompt update

--- src/prompts/parseReport.ts
+++ src/prompts/parseReport.ts
@@ -1,7 +1,7 @@
 const prompt = `
-I have a text extracted from a PDF file containing a company's annual report and need assistance in extracting and analyzing information about their CO2 emissions. Please follow these specific steps:
+I have a text extracted from a file (PDF or website) containing a company's annual report and need assistance in extracting and analyzing information about their CO2 emissions. Please follow these specific steps:
 
-1. **Reading PDF Files**: Read the text extract above. Look for sections containing data on CO2 emissions, specifically focusing on Scope 1 (direct GHG emissions), Scope 2 (indirect GHG emissions, market based (MB) or locatation based (LB)), and Scope 3 emissions (often marked in other unit x1000). Use Ghg protocol as a reference for what to look for. If you interpret tables, please know that some of the values might be empty - take extra care to ensure you are not confusing years when parsing the values.
+1. **Reading Report Files**: Read the text extract above. Look for sections containing data on CO2 emissions, specifically focusing on Scope 1 (direct GHG emissions), Scope 2 (indirect GHG emissions, market based (MB) or locatation based (LB)), and Scope 3 emissions (often marked in other unit x1000). Use Ghg protocol as a reference for what to look for. If you interpret tables, please know that some of the values might be empty - take extra care to ensure you are not confusing years when parsing the values.
 
 2. **Handling Units**: Pay close attention to the units and handle them correctly. If emissions are reported in thousands of tons (x1,000 ton CO2e), make this clear. Mt CO2e means million ton CO2e. If the figures are on a different scale, such as millions of tons (x1,000,000 ton CO2e), note this but never try to convert units. Also look for any side notes or footnotes that may explain the units. Always present the data in json even if there are disclaimers in the footnotes.
 
