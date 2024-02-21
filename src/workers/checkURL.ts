3acb07d4cc1ee4cc745985d487aeacd1b8b16a3e
Elvira <elvbom@users.noreply.github.com>
2024-02-21 23:00:15 +0100

    add discord msgs to new workers

--- src/workers/checkURL.ts
+++ src/workers/checkURL.ts
@@ -1,17 +1,27 @@
 import { Worker, Job } from 'bullmq'
 import redis from '../config/redis'
 import { downloadPDF, downloadWebsite } from '../queues';
+import discord from '../discord';
+import { TextChannel } from 'discord.js';
 
 class JobData extends Job {
   data: {
     url: string
+    channelId: string,
+    messageId: string
   }
 }
 
 const worker = new Worker(
   'checkURL',
   async (job: JobData) => {
-    const { url } = job.data
+    const { url, channelId, messageId } = job.data
+    
+    job.log(`Downloading from url: ${url}`)
+    const channel = await discord.client.channels.fetch(channelId) as TextChannel
+    const message = await channel.messages.fetch(messageId)
+    await message.edit(`Laddar ner PDF...`)
+
     const fileType = url.endsWith('.pdf') ? 'pdf' : 'webpage'
     if (fileType === 'pdf') {
       job.log('Data is PDF: ' + url)
--- src/workers/downloadWebsite.ts
+++ src/workers/downloadWebsite.ts
@@ -3,17 +3,27 @@ import redis from '../config/redis'
 import { splitText } from '../queues'
 import axios from 'axios'
 import { JSDOM } from 'jsdom'
+import { TextChannel } from 'discord.js'
+import discord from '../discord'
 
 class JobData extends Job {
   data: {
     url: string
+    channelId: string,
+    messageId: string
   }
 }
 
 const worker = new Worker(
   'downloadWebsite',
   async (job: JobData) => {
-    const { url } = job.data
+    const { url, channelId, messageId } = job.data
+    
+    job.log(`Downloading from url: ${url}`)
+    const channel = await discord.client.channels.fetch(channelId) as TextChannel
+    const message = await channel.messages.fetch(messageId)
+    await message.edit(`Laddar ner sida...`)
+
     job.log(`Downloading website from url: ${url}`)
 
     let text = ''
@@ -41,8 +51,9 @@ const worker = new Worker(
 
       const cleanedHTML = document.documentElement.outerHTML
       text = cleanedHTML.replace(/\t\t/g, '\n\n')
-    } catch (webError) {
-      console.error(`Error downloading website ${url}: ${webError}`)
+    } catch (error) {
+      await message.edit(`Fel vid nedladdning av sida: ${error.message}`);
+      console.error(`Error downloading website ${url}: ${error}`)
     }
 
     splitText.add('split text ' + text.slice(0, 20), {
