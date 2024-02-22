3ec68a21bc0d101555eb6cdd7f46d36d12d66f24
William Ryder <williamryderb@gmail.com>
2024-02-22 12:06:14 +0100

    update elastic functions

--- src/elastic.ts
+++ src/elastic.ts
@@ -1,65 +1,123 @@
 import config from './config/elasticsearch';
 import { Client } from '@elastic/elasticsearch';
+import * as crypto from 'crypto';
 
 class Elastic {
   client: Client;
   indexName: string;
 
   constructor({ node, indexName }) {
-    this.client = new Client({ node });
-    this.indexName = indexName;
+    try  {
+      this.client = new Client({ node });
+      this.indexName = indexName;
+    } catch (error) {
+      console.error('Elasticsearch constructor error:', error);
+    }
+  }
+
+  // Hash the URL to use as the document ID
+  private hashUrl(url: string): string {
+    return crypto.createHash('sha256').update(url).digest('hex');
   }
 
   async setupIndex() {
-    const indexExists = await this.client.indices.exists({ index: this.indexName });
-    if (!indexExists) {
-      await this.client.indices.create({
-        index: this.indexName,
-        body: {
-          mappings: {
-            properties: {
-              url: { type: 'keyword' },
-              pdf: { type: 'binary' },
-              report: { type: 'object' },
-              state: { type: 'keyword' }
+    try {
+      console.log(`Checking if index ${this.indexName} exists...`);
+      const indexExists = await this.client.indices.exists({ index: this.indexName });
+      if (!indexExists) {
+        await this.client.indices.create({
+          index: this.indexName,
+          body: {
+            mappings: {
+              properties: {
+                url: { type: 'keyword' },
+                pdf: { type: 'binary' },
+                report: { type: 'text' },
+                state: { type: 'keyword' }
+              }
             }
           }
-        }
       });
       console.log(`Index ${this.indexName} created.`);
     } else {
       console.log(`Index ${this.indexName} already exists.`);
     }
+    } catch (error) {
+      console.error('Elasticsearch setupIndex error:', error);
+    }
   }
 
-  async indexDocument(documentId: string, pdfContent: string, reportData: object = null) {
+  async createEntryWithUrl(url: string) {
+    const documentId = this.hashUrl(url);
     const docBody = {
-      pdf: pdfContent,
-      report: reportData,
+      url: url,
       state: 'pending'
     };
 
-    await this.client.index({
-      index: this.indexName,
-      id: documentId,
-      body: docBody
-    });
-
-    console.log(`Document ${documentId} indexed.`);
+    try {
+      await this.client.index({
+        index: this.indexName,
+        id: documentId,
+        body: docBody
+      });
+      console.log(`Entry created with PDF URL. Document ID: ${documentId}`);
+    } catch (error) {
+      console.error(`Error creating entry with PDF URL for Document ID ${documentId}:`, error);
+    }
   }
 
-  async updateDocumentState(documentId: string, newState: string) {
-    await this.client.update({
-      index: this.indexName,
-      id: documentId,
-      body: {
-        doc: {
-          state: newState
+  async addPDFContent(url: string, pdfContent: string) {
+    const documentId = this.hashUrl(url);
+    try {
+      await this.client.update({
+        index: this.indexName,
+        id: documentId,
+        body: {
+          doc: {
+            pdf: pdfContent
+          }
         }
-      }
-    });
+      });
+      console.log(`PDF content added. Document ID: ${documentId}`);
+    } catch (error) {
+      console.error(`Error adding PDF content for Document ID ${documentId}:`, error);
+    }
+  }
 
-    console.log(`Document ${documentId} state updated to ${newState}.`);
+  async addReportData(url: string, reportData: object) {
+    const documentId = this.hashUrl(url);
+    try {
+      await this.client.update({
+        index: this.indexName,
+        id: documentId,
+        body: {
+          doc: {
+            report: reportData
+          }
+        }
+      });
+      console.log(`Report data added. Document ID: ${documentId}`);
+    } catch (error) {
+      console.error(`Error adding report data for Document ID ${documentId}:`, error);
+    }
+  }
+
+  async updateDocumentState(url: string, newState: string) {
+    const documentId = this.hashUrl(url);
+    try {
+      await this.client.update({
+        index: this.indexName,
+        id: documentId,
+        body: {
+          doc: {
+            state: newState
+          }
+        }
+      });
+      console.log(`Document ${documentId} state updated to ${newState}.`);
+    } catch (error) {
+      console.error(`Error updating document state for Document ID ${documentId}:`, error);
+    }
   }
 }
 
