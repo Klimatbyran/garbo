351618c7ee7789512b9bbae207610c95407d0442
Elvira <elvbom@users.noreply.github.com>
2024-03-13 15:16:51 +0100

    move website parsing to try fetch and add elastic to website worker

--- src/workers/downloadWebsite.ts
+++ src/workers/downloadWebsite.ts
@@ -30,47 +30,59 @@ const worker = new Worker(
     let text = ''
 
     try {
-      const response = await axios.get(url)
-      const html = response.data
-      const { document } = new JSDOM(html).window
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Nedladdning misslyckades: ${response.statusText}`);
+      }
+      const buffer = await response.arrayBuffer();
+      let text;
+      try {
+        const html = response.body
+        const { document } = new JSDOM(html).window
 
-      const unnecessaryElements = document.querySelectorAll(
-        'script, style, link, meta, a, p, div'
-      )
-      unnecessaryElements.forEach((element) => {
-        if (
-          element.tagName === 'A' ||
-          element.tagName === 'P' ||
-          element.tagName === 'DIV'
-        ) {
-          const textNode = document.createTextNode(element.textContent || '')
-          element.parentNode?.replaceChild(textNode, element)
-        } else {
-          element.remove()
-        }
-      })
+        const unnecessaryElements = document.querySelectorAll(
+          'script, style, link, meta, a, p, div'
+        )
+        unnecessaryElements.forEach((element) => {
+          if (
+            element.tagName === 'A' ||
+            element.tagName === 'P' ||
+            element.tagName === 'DIV'
+          ) {
+            const textNode = document.createTextNode(element.textContent || '')
+            element.parentNode?.replaceChild(textNode, element)
+          } else {
+            element.remove()
+          }
+        })
 
-      const cleanedHTML = document.documentElement.outerHTML
-      text = cleanedHTML.replace(/\t\t/g, '\n\n')
+        const cleanedHTML = document.documentElement.outerHTML
+        text = cleanedHTML.replace(/\t\t/g, '\n\n')
+      } catch (error) {
+        await message.edit(`Fel vid tolkning av sida: ${error.message}`);
+        job.log(`Error parsing website: ${error.message}`);
+        throw error;
+      }      
+      let reportHash = '';
+      try {
+        reportHash = await elastic.indexPdf(buffer);
+      } catch (error) {
+        job.log(`Error indexing website: ${error.message}`);
+      }
+
+      splitText.add('split text ' + text.slice(0, 20), {
+        url,
+        text,
+        channelId,
+        messageId,
+        pdfHash: reportHash,
+      });
+
+      return text
     } catch (error) {
       await message.edit(`Fel vid nedladdning av sida: ${error.message}`);
       console.error(`Error downloading website ${url}: ${error}`)
     }
-
-    let pdfHash = '';
-    try {
-      pdfHash = await elastic.indexPdf(buffer);
-    } catch (error) {
-      job.log(`Error indexing PDF: ${error.message}`);
-    }
-
-    splitText.add('split text ' + text.slice(0, 20), {
-      url,
-      text,
-      channelId,
-      messageId,
-      pdfHash,
-    })
 
     return text
   },
