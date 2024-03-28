6de1215e2c3e6916651d321446b4cd5002c6a0d8
Christian Landgren <christian@landgren.nu>
2024-03-28 11:30:19 +0100

    feat: convert array to object before sending to elastic

--- src/elastic.ts
+++ src/elastic.ts
@@ -1,33 +1,35 @@
-import config from './config/elasticsearch';
-import { Client } from '@elastic/elasticsearch';
-import * as crypto from 'crypto';
+import config from './config/elasticsearch'
+import { Client } from '@elastic/elasticsearch'
+import * as crypto from 'crypto'
 
 class Elastic {
-  client: Client;
-  indexName: string;
-  pdfIndex: string;
+  client: Client
+  indexName: string
+  pdfIndex: string
 
   constructor({ node, indexName }) {
-    try  {
-      this.client = new Client({ node });
-      this.indexName = indexName;
-      this.pdfIndex = 'pdfs';
+    try {
+      this.client = new Client({ node })
+      this.indexName = indexName
+      this.pdfIndex = 'pdfs'
     } catch (error) {
-      console.error('Node URL:', node);
-      console.error('Index name:', indexName);
-      console.error('Elasticsearch constructor error:', error);
+      console.error('Node URL:', node)
+      console.error('Index name:', indexName)
+      console.error('Elasticsearch constructor error:', error)
     }
   }
 
   async setupIndices() {
-    await this.createEmissionsIndex();
-    await this.createPdfIndex();
+    await this.createEmissionsIndex()
+    await this.createPdfIndex()
   }
 
   private async createPdfIndex() {
     try {
-      console.log(`Checking if index ${this.pdfIndex} exists...`);
-      const indexExists = await this.client.indices.exists({ index: this.pdfIndex });
+      console.log(`Checking if index ${this.pdfIndex} exists...`)
+      const indexExists = await this.client.indices.exists({
+        index: this.pdfIndex,
+      })
       if (!indexExists) {
         await this.client.indices.create({
           index: this.pdfIndex,
@@ -35,23 +37,25 @@ class Elastic {
             mappings: {
               properties: {
                 pdf: { type: 'binary' },
-              }
-            }
-          }
-        });
-        console.log(`Index ${this.pdfIndex} created.`);
+              },
+            },
+          },
+        })
+        console.log(`Index ${this.pdfIndex} created.`)
       } else {
-        console.log(`Index ${this.pdfIndex} already exists.`);
+        console.log(`Index ${this.pdfIndex} already exists.`)
       }
     } catch (error) {
-      console.error('Elasticsearch pdfIndex error:', error);
+      console.error('Elasticsearch pdfIndex error:', error)
     }
-  } 
+  }
 
   private async createEmissionsIndex() {
     try {
-      console.log(`Checking if index ${this.indexName} exists...`);
-      const indexExists = await this.client.indices.exists({ index: this.indexName });
+      console.log(`Checking if index ${this.indexName} exists...`)
+      const indexExists = await this.client.indices.exists({
+        index: this.indexName,
+      })
       if (!indexExists) {
         await this.client.indices.create({
           index: this.indexName,
@@ -61,7 +65,7 @@ class Elastic {
                 url: { type: 'keyword' },
                 pdfHash: { type: 'keyword' },
                 report: {
-                  type: 'nested',
+                  type: 'object',
                   properties: {
                     companyName: { type: 'keyword' },
                     orgranizationNumber: { type: 'keyword' },
@@ -69,119 +73,154 @@ class Elastic {
                     baseYear: { type: 'keyword' },
                     url: { type: 'keyword' },
                     emissions: {
-                      type: 'nested',
+                      type: 'object',
                       properties: {
-                        year: { type: 'keyword' },
-                        scope1: {
+                        '*': {
+                          type: 'object',
                           properties: {
-                            emissions: { type: 'double' },
-                            unit: { type: 'keyword' }
-                          }
-                        },
-                        scope2: {
-                          properties: {
-                            emissions: { type: 'double' },
-                            unit: { type: 'keyword' },
-                            mb: { type: 'double' },
-                            lb: { type: 'double' }
-                          }
-                        },
-                        scope3: {
-                          properties: {
-                            emissions: { type: 'double' },
-                            unit: { type: 'keyword' },
-                            baseYear: { type: 'keyword' },
-                            categories: {
+                            year: { type: 'keyword' },
+                            scope1: {
                               properties: {
-                                "1_purchasedGoods": { type: 'double' },
-                                "2_capitalGoods": { type: 'double' },
-                                "3_fuelAndEnergyRelatedActivities": { type: 'double' },
-                                "4_upstreamTransportationAndDistribution": { type: 'double' },
-                                "5_wasteGeneratedInOperations": { type: 'double' },
-                                "6_businessTravel": { type: 'double' },
-                                "7_employeeCommuting": { type: 'double' },
-                                "8_upstreamLeasedAssets": { type: 'double' },
-                                "9_downstreamTransportationAndDistribution": { type: 'double' },
-                                "10_processingOfSoldProducts": { type: 'double' },
-                                "11_useOfSoldProducts": { type: 'double' },
-                                "12_endOfLifeTreatmentOfSoldProducts": { type: 'double' },
-                                "13_downstreamLeasedAssets": { type: 'double' },
-                                "14_franchises": { type: 'double' },
-                                "15_investments": { type: 'double' },
-                                "16_other": { type: 'double' }
-                              }
-                            }
-                          }
+                                emissions: { type: 'double' },
+                                unit: { type: 'keyword' },
+                              },
+                            },
+                            scope2: {
+                              properties: {
+                                emissions: { type: 'double' },
+                                unit: { type: 'keyword' },
+                                mb: { type: 'double' },
+                                lb: { type: 'double' },
+                              },
+                            },
+                            scope3: {
+                              properties: {
+                                emissions: { type: 'double' },
+                                unit: { type: 'keyword' },
+                                baseYear: { type: 'keyword' },
+                                categories: {
+                                  properties: {
+                                    '1_purchasedGoods': { type: 'double' },
+                                    '2_capitalGoods': { type: 'double' },
+                                    '3_fuelAndEnergyRelatedActivities': {
+                                      type: 'double',
+                                    },
+                                    '4_upstreamTransportationAndDistribution': {
+                                      type: 'double',
+                                    },
+                                    '5_wasteGeneratedInOperations': {
+                                      type: 'double',
+                                    },
+                                    '6_businessTravel': { type: 'double' },
+                                    '7_employeeCommuting': { type: 'double' },
+                                    '8_upstreamLeasedAssets': {
+                                      type: 'double',
+                                    },
+                                    '9_downstreamTransportationAndDistribution':
+                                      {
+                                        type: 'double',
+                                      },
+                                    '10_processingOfSoldProducts': {
+                                      type: 'double',
+                                    },
+                                    '11_useOfSoldProducts': { type: 'double' },
+                                    '12_endOfLifeTreatmentOfSoldProducts': {
+                                      type: 'double',
+                                    },
+                                    '13_downstreamLeasedAssets': {
+                                      type: 'double',
+                                    },
+                                    '14_franchises': { type: 'double' },
+                                    '15_investments': { type: 'double' },
+                                    '16_other': { type: 'double' },
+                                  },
+                                },
+                              },
+                            },
+                          },
                         },
                         totalEmissions: { type: 'double' },
                         totalUnit: { type: 'keyword' },
-                      }
+                      },
                     },
                     reliability: { type: 'keyword' },
                     needsReview: { type: 'boolean' },
                     reviewComment: { type: 'text' },
-                    reviewStatusCode: { type: 'keyword' }
-                  }
+                    reviewStatusCode: { type: 'keyword' },
+                  },
                 },
                 state: { type: 'keyword' },
-                timestamp: { type: 'date' }
-              }
-            }
-          }
-      });
-      console.log(`Index ${this.indexName} created.`);
-    } else {
-      console.log(`Index ${this.indexName} already exists.`);
-    }
+                timestamp: { type: 'date' },
+              },
+            },
+          },
+        })
+        console.log(`Index ${this.indexName} created.`)
+      } else {
+        console.log(`Index ${this.indexName} already exists.`)
+      }
     } catch (error) {
-      console.error('Elasticsearch setupIndex error:', error);
+      console.error('Elasticsearch setupIndex error:', error)
     }
   }
 
   public hashPdf(pdfBuffer: Buffer): string {
-    return crypto.createHash('sha256').update(pdfBuffer).digest('hex');
+    return crypto.createHash('sha256').update(pdfBuffer).digest('hex')
   }
 
   // Index the PDF using the hash as document ID and returning it for reference
   async indexPdf(pdfBuffer: ArrayBuffer) {
-    const buffer = Buffer.from(pdfBuffer);
-    const pdfHash = this.hashPdf(buffer);
-  
+    const buffer = Buffer.from(pdfBuffer)
+    const pdfHash = this.hashPdf(buffer)
+
     try {
-      const encodedPdf = buffer.toString('base64');
+      const encodedPdf = buffer.toString('base64')
       await this.client.index({
         index: this.pdfIndex,
         id: pdfHash,
         body: {
           pdf: encodedPdf,
-        }
-      });
-      console.log(`PDF indexed. Document ID: ${pdfHash}`);
-      return pdfHash;
+        },
+      })
+      console.log(`PDF indexed. Document ID: ${pdfHash}`)
+      return pdfHash
     } catch (error) {
-      console.error(`Error indexing PDF for Document ID ${pdfHash}:`, error);
+      console.error(`Error indexing PDF for Document ID ${pdfHash}:`, error)
       // return anyway, as the report is still added later
-      return pdfHash;
+      return pdfHash
     }
   }
 
   async indexReport(pdfHash: string, reportData: string, url: string) {
     try {
+      const parsed = JSON.parse(reportData)
+
+      // Convert from array to object for easier access in elastic
+      const emissions = parsed.emissions.reduce((acc, curr) => {
+        acc[curr.year] = curr
+        return acc
+      }, {})
+
+      const report = {
+        ...parsed,
+        emissions,
+      }
+
       const response = await this.client.index({
         index: this.indexName,
         body: {
           url: url,
           pdfHash: pdfHash,
-          report: reportData,
+          report,
           state: 'pending',
-          timestamp: new Date()
-        }
-      });
-      const documentId = response._id;
-      console.log(`Report data added. Document ID: ${documentId}`);
-      return documentId;
+          timestamp: new Date(),
+        },
+      })
+      const documentId = response._id
+      console.log(`Report data added. Document ID: ${documentId}`)
+      return documentId
     } catch (error) {
-      console.error(`Error adding report data:`, error);
+      console.error(`Error adding report data:`, error)
     }
   }
 
@@ -192,55 +231,59 @@ class Elastic {
         id: documentId,
         body: {
           doc: {
-            state: newState
-          }
-        }
-      });
-      console.log(`Document ${documentId} state updated to ${newState}.`);
+            state: newState,
+          },
+        },
+      })
+      console.log(`Document ${documentId} state updated to ${newState}.`)
     } catch (error) {
-      console.error(`Error updating document state for Document ID ${documentId}:`, error);
+      console.error(
+        `Error updating document state for Document ID ${documentId}:`,
+        error
+      )
     }
   }
 
   // TODO support report per year and company (not only latest approved). So; get the latest approved report for each company and year
   async getAllLatestApprovedReports() {
     try {
-      const { body } = await this.client.search({
+      const { body } = (await this.client.search({
         index: this.indexName,
         body: {
           query: {
             bool: {
-              must: [
-                { match: { state: 'approved' } }
-              ]
-            }
+              must: [{ match: { state: 'approved' } }],
+            },
           },
           sort: [
             { 'report.companyName.keyword': { order: 'asc' } },
-            { timestamp: { order: 'desc' } }
+            { timestamp: { order: 'desc' } },
           ],
-          size: 1000
-        }
-      }) as any;
-  
-      const reports = body.hits.hits.map(hit => hit._source);
-      const latestReports = {};
-  
-      reports.forEach(report => {
-        const companyName = report.report.companyName;
+          size: 1000,
+        },
+      })) as any
+
+      const reports = body.hits.hits.map((hit) => hit._source)
+      const latestReports = {}
+
+      reports.forEach((report) => {
+        const companyName = report.report.companyName
         // If the company hasn't been added to latestReports or the current report is more recent, update it
-        if (!latestReports[companyName] || new Date(latestReports[companyName].timestamp) < new Date(report.timestamp)) {
-          latestReports[companyName] = report;
+        if (
+          !latestReports[companyName] ||
+          new Date(latestReports[companyName].timestamp) <
+            new Date(report.timestamp)
+        ) {
+          latestReports[companyName] = report
         }
-      });
-  
-      return Object.values(latestReports);
+      })
+
+      return Object.values(latestReports)
     } catch (error) {
-      console.error('Error retrieving all approved reports:', error);
-      return [];
+      console.error('Error retrieving all approved reports:', error)
+      return []
     }
   }
-
 }
 
 export default new Elastic(config)
