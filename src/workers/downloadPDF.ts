6a6c327d5f09cdd03bc1a751bd2ba20fa58b79ee
Christian Landgren <christian@landgren.nu>
2024-04-23 16:12:17 +0200

    fix: use a normal user agent to prevent errors

--- src/workers/downloadPDF.ts
+++ src/workers/downloadPDF.ts
@@ -29,7 +29,8 @@ const worker = new Worker(
     try {
       const response = await fetch(url, {
         headers: {
-          'User-Agent': 'Garbo/1.0 (Linux; OpenAI 4;) Klimatkollen',
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36', //Garbo/1.0 (Linux; OpenAI 4;) Klimatkollen',
         },
       })
       if (!response.ok) {
