f6f707c66e8a8871f37bbcc9cbe3ad04f2f06145
William Ryder <williamryderb@gmail.com>
2024-04-26 10:49:21 +0200

    fix: feedback modal

--- src/discord/interactions/feedback.ts
+++ src/discord/interactions/feedback.ts
@@ -1,8 +1,31 @@
-import { EmbedBuilder } from 'discord.js'
+import {
+  ActionRowBuilder,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle
+} from 'discord.js'
 import { userFeedback } from '../../queues'
 
 export default {
   async execute(interaction, job) {
+    const input = new TextInputBuilder()
+    .setCustomId('editInput')
+    .setLabel(`Granska utsläppsdata`)
+    .setStyle(TextInputStyle.Paragraph)
+
+  const actionRow =
+    new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+      input
+    )
+
+  const modal = new ModalBuilder()
+    .setCustomId('editModal')
+    .setTitle(`Granska data för...`) // ${parsedJson.companyName}`)
+    .addComponents(actionRow)
+
+  await interaction.showModal(modal)
+
     const submitted = await interaction
       .awaitModalSubmit({
         time: 60000 * 20, // user has to submit the modal within 20 minutes
