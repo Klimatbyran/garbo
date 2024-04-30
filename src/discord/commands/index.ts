11ae65b9ee143d515c593b5233f61628c7ad216e
Christian Landgren <christian@landgren.nu>
2024-04-30 23:29:35 +0200

    This needs to also be updated when removing or adding commands

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,6 +1,5 @@
 import ping from './ping'
-import pdf from './pdf'
 import pdfs from './pdfs'
 import parse from './parse'
 
-export default [ping, pdf, pdfs, parse]
+export default [ping, pdfs, parse]
