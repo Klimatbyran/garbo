4438f9176c4d24c1be52af879e818d44f44df86d
Christian Landgren <christian@landgren.nu>
2024-05-10 13:29:48 +0200

    Update index.ts

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,7 +1,6 @@
 import ping from './ping'
 import pdfs from './pdfs'
 import parse from './parse'
-import list from './list'
 import facit from './facit'
 
-export default [ping, pdfs, parse, list, facit]
+export default [ping, pdfs, parse, facit]
