44c61921d100d9b5874f1e7b3409410b9b69a4e2
Christian Landgren <christian@landgren.nu>
2024-05-23 14:34:16 +0200

    fix: never calculate scope1+2 and use tCO2e

--- src/prompts/followUp/scope12.ts
+++ src/prompts/followUp/scope12.ts
@@ -1,6 +1,8 @@
 const scope12 = `
 Extract scope 1 and 2 emissions according to the GHG protocol (CO2e). Include all years you can find and never exclude latest year.
-Include market based and location based in scope 2. Add it as field emissions:
+Include market based and location based in scope 2. Always use tonnes CO2e as unit. Add it as field emissions:
+
+NEVER CALCULATE ANY EMISSIONS. ONLY REPORT THE DATA AS IT IS IN THE PDF. If you can't find any data or if you are uncertain, report it as null.
 
 Example - feel free to add more fields and relevant data:
 \`\`\`json
@@ -9,12 +11,12 @@ Example - feel free to add more fields and relevant data:
     {
       "year": 2021,
       "scope1": {
-        emissions: 40,3,
+        emissions: 40.3,
         unit: "tCO2e"
       },
       "scope2": {
-        mb: 10,4,
-        lb: 14,5,
+        mb: 10.4,
+        lb: 14.5,
         unit: "tCO2e"
       }
     },
