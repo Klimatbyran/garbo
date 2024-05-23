652135f50147629275cce663af7ad5817c4af64f
Christian Landgren <christian@landgren.nu>
2024-05-23 11:36:13 +0200

    fix: add ghg categories

--- src/prompts/followUp/scope3.ts
+++ src/prompts/followUp/scope3.ts
@@ -1,6 +1,27 @@
 const scope3 = `
 Extract scope 3 emissions according to the GHG protocol. Add it as field emissions per year. Include all years you can find and never exclude latest year. Include as many categories as you can find and their scope 3 emissions.
 
+Important! Always report according to the offical GHG categories. If you can't find the corresponding category, report it as "other".
+
+1_purchasedGoods
+2_capitalGoods
+3_fuelAndEnergyRelatedActivities
+4_upstreamTransportationAndDistribution
+5_wasteGeneratedInOperations
+6_businessTravel
+7_employeeCommuting
+8_upstreamLeasedAssets
+9_downstreamTransportationAndDistribution
+10_processingOfSoldProducts
+11_useOfSoldProducts
+12_endOfLifeTreatmentOfSoldProducts
+13_downstreamLeasedAssets
+14_franchises
+15_investments
+16_other
+
+
+
 Example - feel free to add more fields and relevant data:
 
 \`\`\`json
