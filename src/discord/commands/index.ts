2e60d0a92ada82357c5faeee086deb89aa891516
Christian Landgren <christian@landgren.nu>
2024-06-12 17:16:37 +0200

    fix: rename list to all

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,8 +1,8 @@
 import pdfs from './pdfs'
 import parse from './parse'
-import list from './all'
+import all from './all'
 import facit from './facit'
 import approve from './approve'
 import json from './json'
 
-export default [facit, pdfs, parse, list, approve, json]
+export default [facit, pdfs, parse, all, approve, json]
