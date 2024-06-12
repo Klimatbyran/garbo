c444bfba781bdc5e6169698ee85783d3adfcd856
Christian Landgren <christian@landgren.nu>
2024-06-12 22:14:11 +0200

    fix: more swedish

--- src/prompts/followUp/goals.ts
+++ src/prompts/followUp/goals.ts
@@ -2,12 +2,12 @@ const goals = `
 Extract the company goals for reducing their carbon emissions add it as field goals.
 Be as accurate as possible when extracting goals. These values will be plotted in a graph later on. Only keep max 3-5 most relevant goals, if there are too many to fit in the field, only keep the most important ones.
 
-** WRITE IN SWEDISH **
+** LANGUAGE: WRITE IN SWEDISH. If text is in english, translate to Swedish **
 
 \`\`\`json
 { 
   "goals": [
-    { description: 'Net zero innan xxx.', 
+    { description: 'Minska utsläppen med 50%', 
       year: xxx, 
       reductionPercent: 100
     }
