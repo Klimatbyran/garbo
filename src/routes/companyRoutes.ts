6c60d9493ade0dbecf597027947dd00ce8f7e1e9
Christian Landgren <christian@landgren.nu>
2024-06-13 16:26:59 +0200

    feat: add cache for api

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,20 +1,26 @@
 import express, { Request, Response } from 'express'
 import opensearch from '../opensearch'
+const apicache = require('apicache')
 
+let cache = apicache.middleware
 const router = express.Router()
 
-router.get('/companies', async (req: Request, res: Response) => {
-  try {
-    const reports = await opensearch.getAllLatestApprovedReports()
-    if (reports) {
-      res.json(reports)
-    } else {
-      res.status(404).send('Company emission reports not found')
+router.get(
+  '/companies',
+  cache('5 minutes'),
+  async (req: Request, res: Response) => {
+    try {
+      const reports = await opensearch.getAllLatestApprovedReports()
+      if (reports) {
+        res.json(reports)
+      } else {
+        res.status(404).send('Company emission reports not found')
+      }
+    } catch (error) {
+      console.error('Failed to fetch company emission reports:', error)
+      res.status(500).json({ error: 'Error fetching company emission reports' })
     }
-  } catch (error) {
-    console.error('Failed to fetch company emission reports:', error)
-    res.status(500).json({ error: 'Error fetching company emission reports' })
   }
-})
+)
 
 export default router
