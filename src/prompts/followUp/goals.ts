51e7564f4c9b1291a26d2a483244ea54180d5189
Christian Landgren <christian@landgren.nu>
2024-06-25 09:02:28 +0200

    fix: Never guess year. Solves #164

--- src/prompts/followUp/goals.ts
+++ src/prompts/followUp/goals.ts
@@ -3,7 +3,9 @@ Extract the company goals for reducing their carbon emissions add it as field go
 Be as accurate as possible when extracting goals. These values will be plotted in a graph later on.
 
 
-Prioritize the list and only include the most important goals. If the list is long, only include max three most important ones.
+Prioritize the list and only include the most important goals. If the list is long, only include max three most important ones (prioritize climate goals).
+
+If no year is mentioned, set year to null.
 
 ** LANGUAGE: WRITE IN SWEDISH. If text is in english, translate to Swedish **
 
