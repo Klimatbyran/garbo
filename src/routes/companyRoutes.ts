2341d11396d3d52076ed384646f8c70eba9f639e
Christian Landgren <christian@landgren.nu>
2024-08-15 17:04:03 +0200

    feat: change api to return new database rows

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from 'express'
 import opensearch from '../opensearch'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const router = express.Router()
 
 router.get('/companies', async (req: Request, res: Response) => {
   try {
-    const reports = await opensearch.getAllLatestApprovedReports()
-    if (reports) {
-      res.json(reports)
-    } else {
-      res.status(404).send('Company emission reports not found')
-    }
+    // TODO: get all companies
+    const companies = await prisma.company.findMany()
+    res.json(companies)
   } catch (error) {
     console.error('Failed to fetch company emission reports:', error)
     res.status(500).json({ error: 'Error fetching company emission reports' })
