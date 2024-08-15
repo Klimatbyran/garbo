d80f9d93caf8d24266c7205028b8bf9e628f8dcd
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-08-15 14:36:29 +0200

    Update Prisma schema and format using the Prisma formatter

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -1,122 +1,115 @@
 model Company {
-  id             Int           @id @default(autoincrement())
-  companyName    String
+  id             Int          @id @default(autoincrement())
+  name           String       @unique
   description    String
-  wikidataId     String
-  industryGicsId Int           @relation(fields: [industryGicsId], references: [id])
-  industryNaceId Int?          @relation(fields: [industryNaceId], references: [id])
+  /// wikidataId is primarily used to export to or import from wikidata
+  wikidataId     String?
+  /// Company website URL
   url            String
   fiscalYears    FiscalYear[]
+  industryGicsId Int
 
-  industryGics   IndustryGics  @relation(fields: [industryGicsId], references: [id])
-  industryNace   IndustryNace? @relation(fields: [industryNaceId], references: [id])
+  // Is this relationship correct?
+  // Should companies be allowed to have several industries?
+  industryGics IndustryGics @relation(fields: [industryGicsId], references: [id])
 }
 
 model IndustryGics {
-  id               Int        @id @default(autoincrement())
-  name             String
-  sectorCode       String
-  sectorName       String
-  groupCode        String
-  groupName        String
-  industryCode     String
-  industryName     String
-  subIndustryCode  String
-  subIndustryName  String
-  companies        Company[]
-}
-
-model IndustryNace {
-  id               Int        @id @default(autoincrement())
-  sectionCode      String
-  sectionName      String
-  divisionCode     String
-  divisionName     String
-  companies        Company[]
+  id              Int       @id @default(autoincrement())
+  name            String
+  sectorCode      String
+  sectorName      String
+  groupCode       String
+  groupName       String
+  industryCode    String
+  industryName    String
+  subIndustryCode String
+  subIndustryName String
+  companies       Company[]
 }
 
 model FiscalYear {
-  id           Int           @id @default(autoincrement())
-  year         String
-  companyId    Int           @relation(fields: [companyId], references: [id])
-  emissions    Emission?
-  economy      Economy?
-  
-  company      Company       @relation(fields: [companyId], references: [id])
+  id        Int        @id @default(autoincrement())
+  year      String
+  companyId Int
+  emissions Emissions?
+  economy   Economy?
+
+  company Company @relation(fields: [companyId], references: [id])
 }
 
-model Emission {
-  id            Int           @id @default(autoincrement())
-  scope1Id      Int           @relation(fields: [scope1Id], references: [id])
-  scope2Id      Int           @relation(fields: [scope2Id], references: [id])
-  scope3Id      Int           @relation(fields: [scope3Id], references: [id])
-  fiscalYearId  Int           @relation(fields: [fiscalYearId], references: [id])
+/// Reported emissions for a specific fiscalYear
+model Emissions {
+  id           Int @id @default(autoincrement())
+  fiscalYearId Int
+  scope1Id     Int
+  scope2Id     Int
+  scope3Id     Int
 
-  scope1        Scope1
-  scope2        Scope2
-  scope3        Scope3
-
-  fiscalYear    FiscalYear    @relation(fields: [fiscalYearId], references: [id])
+  fiscalYear FiscalYear @relation(fields: [fiscalYearId], references: [id])
+  scope1     Scope1     @relation(fields: [scope1Id], references: [id])
+  scope2     Scope2     @relation(fields: [scope2Id], references: [id])
+  scope3     Scope3     @relation(fields: [scope3Id], references: [id])
 }
 
 model Scope1 {
-  id        Int      @id @default(autoincrement())
+  id        Int    @id @default(autoincrement())
   emissions Float
   biogenic  Float?
   unit      String
   baseYear  String
-  sourceId  Int      @relation(fields: [sourceId], references: [id])
+  sourceId  Int    @relation(fields: [sourceId], references: [id])
 
-  source    Source   @relation(fields: [sourceId], references: [id])
+  source Source @relation(fields: [sourceId], references: [id])
 }
 
 model Scope2 {
-  id        Int      @id @default(autoincrement())
+  id        Int    @id @default(autoincrement())
   emissions Float
   biogenic  Float?
   unit      String
   mb        Float?
   lb        Float?
   baseYear  String
-  sourceId  Int      @relation(fields: [sourceId], references: [id])
+  sourceId  Int    @relation(fields: [sourceId], references: [id])
 
-  source    Source   @relation(fields: [sourceId], references: [id])
+  source Source @relation(fields: [sourceId], references: [id])
 }
 
 model Scope3 {
-  id        Int      @id @default(autoincrement())
+  id        Int    @id @default(autoincrement())
   emissions Float?
   biogenic  Float?
   unit      String
   baseYear  String
-  sourceId  Int      @relation(fields: [sourceId], references: [id])
+  sourceId  Int    @relation(fields: [sourceId], references: [id])
 
   categories Scope3Category[]
 
-  source    Source   @relation(fields: [sourceId], references: [id])
+  source Source @relation(fields: [sourceId], references: [id])
 }
 
 model Scope3Category {
-  id            Int              @id @default(autoincrement())
-  category      Scope3CategoryEnum
-  emissions     Float?
-  scope3Id      Int              @relation(fields: [scope3Id], references: [id])
-  sourceId      Int              @relation(fields: [sourceId], references: [id])
+  id        Int                @id @default(autoincrement())
+  category  Scope3CategoryEnum
+  emissions Float?
+  scope3Id  Int                @relation(fields: [scope3Id], references: [id])
+  sourceId  Int                @relation(fields: [sourceId], references: [id])
 
-  scope3        Scope3           @relation(fields: [scope3Id], references: [id])
-  source        Source           @relation(fields: [sourceId], references: [id])
+  scope3 Scope3 @relation(fields: [scope3Id], references: [id])
+  source Source @relation(fields: [sourceId], references: [id])
 }
 
 model Economy {
-  id           Int           @id @default(autoincrement())
+  id           Int    @id @default(autoincrement())
   turnover     Float
   unit         String
   employees    Int
-  sourceId     Int           @relation(fields: [sourceId], references: [id])
-  fiscalYearId Int           @relation(fields: [fiscalYearId], references: [id])
+  sourceId     Int    @relation(fields: [sourceId], references: [id])
+  fiscalYearId Int    @relation(fields: [fiscalYearId], references: [id])
 
-  fiscalYear   FiscalYear    @relation(fields: [fiscalYearId], references: [id])
-  source       Source        @relation(fields: [sourceId], references: [id])
+  fiscalYear FiscalYear @relation(fields: [fiscalYearId], references: [id])
+  source     Source     @relation(fields: [sourceId], references: [id])
 }
 
 model Goal {
@@ -127,7 +120,7 @@ model Goal {
   baseYear    String
   companyId   Int     @relation(fields: [companyId], references: [id])
 
-  company     Company @relation(fields: [companyId], references: [id])
+  company Company @relation(fields: [companyId], references: [id])
 }
 
 model Initiative {
@@ -138,27 +131,19 @@ model Initiative {
   scope       String
   companyId   Int     @relation(fields: [companyId], references: [id])
 
-  company     Company @relation(fields: [companyId], references: [id])
-}
-
-model Wikidata {
-  id          Int      @id @default(autoincrement())
-  node        String
-  url         String
-  logo        String
-  label       String
-  description String
-  emissions   Json[]
+  company Company @relation(fields: [companyId], references: [id])
 }
 
 model Source {
-  id         Int      @id @default(autoincrement())
-  url        String?
-  comment    String?
-  userId     Int
-  lastUpdated DateTime @default(now())
+  id          Int      @id @default(autoincrement())
+  url         String?
+  comment     String?
+  userId      Int
+  lastUpdated DateTime @default(now()) @updatedAt
 }
 
+// Since the enum values are represented as integers,
+// we will be able to use that to determine which category it is
 enum Scope3CategoryEnum {
   purchasedGoods
   capitalGoods
