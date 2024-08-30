55992dcfbb48315ed78d251a136fb380c570ae46
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-08-30 09:50:40 +0200

    Add companies from backup

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,20 +1,22 @@
 import express, { Request, Response } from 'express'
 import opensearch from '../opensearch'
+import { resolve } from 'path'
 
 const router = express.Router()
 
 router.get('/companies', async (req: Request, res: Response) => {
-  try {
-    const reports = await opensearch.getAllLatestApprovedReports()
-    if (reports) {
-      res.json(reports)
-    } else {
-      res.status(404).send('Company emission reports not found')
-    }
-  } catch (error) {
-    console.error('Failed to fetch company emission reports:', error)
-    res.status(500).json({ error: 'Error fetching company emission reports' })
-  }
+  res.sendFile(resolve('./src/data/companies.json'))
+  // try {
+  //   const reports = await opensearch.getAllLatestApprovedReports()
+  //   if (reports) {
+  //     res.json(reports)
+  //   } else {
+  //     res.status(404).send('Company emission reports not found')
+  //   }
+  // } catch (error) {
+  //   console.error('Failed to fetch company emission reports:', error)
+  //   res.status(500).json({ error: 'Error fetching company emission reports' })
+  // }
 })
 
 router.get('/companies/:wikidataId', async (req: Request, res: Response) => {
