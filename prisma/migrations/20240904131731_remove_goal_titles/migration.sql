c7faba5aaaad034d831fa3134929c84b4622a3e0
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-09-04 15:19:05 +0200

    Add and then remove goal titles (which did not exist in the previous DB structure)

---
+++ prisma/migrations/20240904131731_remove_goal_titles/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `Goal` table. All the data in the column will be lost.
+  - Made the column `description` on table `Goal` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Goal" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "year" TEXT,
+    "target" REAL,
+    "baseYear" TEXT,
+    "metadataId" INTEGER NOT NULL,
+    "companyId" TEXT NOT NULL,
+    "reportingPeriodId" INTEGER,
+    CONSTRAINT "Goal_metadataId_fkey" FOREIGN KEY ("metadataId") REFERENCES "metadata" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Goal_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company" ("wikidataId") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Goal_reportingPeriodId_fkey" FOREIGN KEY ("reportingPeriodId") REFERENCES "ReportingPeriod" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Goal" ("baseYear", "companyId", "description", "id", "metadataId", "reportingPeriodId", "target", "year") SELECT "baseYear", "companyId", "description", "id", "metadataId", "reportingPeriodId", "target", "year" FROM "Goal";
+DROP TABLE "Goal";
+ALTER TABLE "new_Goal" RENAME TO "Goal";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
