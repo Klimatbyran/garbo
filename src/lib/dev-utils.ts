d871266822d6eb3b173c181235b4188ef1007015
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-09-19 17:43:05 +0200

    Improve resetDB() helper to both delete data and reset sqlite_sequence to make sure new autoincrement() id:s restart from 1

--- src/lib/dev-utils.ts
+++ src/lib/dev-utils.ts
@@ -3,30 +3,23 @@ import { prisma } from './prisma'
 import { exec } from 'child_process'
 
 export async function resetDB() {
-  // NOTE: The order is very important, and connected to the way relations are set up.
-  // Start by deleting the tables that have relations to others
-  // Finally, delete the tables that are connected to many other ones
-  await prisma.$transaction([
-    prisma.goal.deleteMany(),
-    prisma.initiative.deleteMany(),
-    prisma.scope1.deleteMany(),
-    prisma.scope2.deleteMany(),
-    prisma.statedTotalEmissions.deleteMany(),
-    prisma.scope1And2.deleteMany(),
-    prisma.emissions.deleteMany(),
-    prisma.biogenicEmissions.deleteMany(),
-    prisma.baseYear.deleteMany(),
-    prisma.turnover.deleteMany(),
-    prisma.economy.deleteMany(),
-    prisma.scope3Category.deleteMany(),
-    prisma.scope3.deleteMany(),
-    prisma.industry.deleteMany(),
-    prisma.industryGics.deleteMany(),
-    prisma.reportingPeriod.deleteMany(),
-    prisma.company.deleteMany(),
-    prisma.metadata.deleteMany(),
-    prisma.user.deleteMany(),
-  ])
+  const allTables = await prisma.$queryRaw<
+    { name: string }[]
+  >`SELECT name FROM sqlite_master WHERE type='table';`
+
+  const systemTables = ['sqlite_sequence', '_prisma_migrations']
+  const tables = allTables.filter(({ name }) => !systemTables.includes(name))
+
+  try {
+    for (const { name } of tables) {
+      await prisma.$executeRawUnsafe(`DELETE FROM ${name};`)
+      await prisma.$executeRawUnsafe(
+        `DELETE FROM sqlite_sequence WHERE name='${name}';`
+      )
+    }
+  } catch (error) {
+    console.log({ error })
+  }
 
   await promisify(exec)(`npx prisma db seed`, {
     env: process.env,
