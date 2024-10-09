c28e0e9b7694885dd5f555f8bc49b47c1a796a31
Christian Landgren <christian@landgren.nu>
2024-10-09 13:14:46 +0200

    fix: remove factors and check fiscalYear for inconsistencies

--- src/prompts/followUp/fiscalYear.ts
+++ src/prompts/followUp/fiscalYear.ts
@@ -1,15 +1,25 @@
-// TODO: This needs to be updated to handle reporting periods (which is similar but not exactly the same as fiscal years).
+import { z } from 'zod'
 
-const fiscalYear = `
-Extract the company fiscal year. Sometimes companies have broken fiscal year.  Example: 1 apr -> 31 mar means: startMonth = 4, endMonth = 3. Standard is 1 jan -> 31 dec. 
+const schema = z.object({
+  startMonth: z.number(),
+  endMonth: z.number(),
+})
+
+const prompt = `
+Extract the company fiscal year. Sometimes companies have fiscal year which does not align with the calendar year.
+
+Example: 1 apr -> 31 mar means:
+startMonth = 4, endMonth = 3.
+
+Standard is 1 jan -> 31 dec which is default and if nothing is mentioned in the report, please return these as default.
 
 Example:
 \`\`\`json
 {
-  "companyName": "Company AB",
-  "fiscalYear": {startMonth: 1, endMonth: 12}
+  "startMonth": 1,
+  "endMonth": 12
 }
 \`\`\`
 `
 
-export default fiscalYear
+export { schema, prompt }
