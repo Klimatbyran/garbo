587c72e044e52e992c644d6b48f47dac98a7d43f
Christian Landgren <christian@landgren.nu>
2024-10-09 16:46:30 +0200

    refactor: Update DiscordWorker to support optional job data properties

--- src/lib/DiscordWorker.ts
+++ src/lib/DiscordWorker.ts
@@ -1,46 +1,51 @@
-import { Worker, WorkerOptions, Job as BaseJob } from 'bullmq'
+import { Worker, WorkerOptions, Job as BaseJob, Processor } from 'bullmq'
 import redis from '../config/redis'
 import discord from '../discord'
 
 // NOTE: Maybe this interface could be a class extending the base Job class.
 // Then we would get better type completion
 export type DiscordWorkerJobData = {
-  threadId: string
+  threadId?: string
   previousAnswer?: string
-  messageId: string
+  messageId?: string
 }
 
 class DiscordJob extends BaseJob {
   async sendMessage(message: string) {
     try {
-      const messageId = (await discord.sendMessage({ threadId: this.data.threadId }, message)).id;
-      this.updateData({ messageId });
+      const { id } = await discord.sendMessage(
+        { threadId: this.data.threadId },
+        message
+      )
+      this.updateData({ messageId: id })
     } catch (err) {
-      console.error(`Error sending message: ${err.message}`);
+      console.error(`Error sending message: ${err.message}`)
     }
   }
 
   async editMessage(editedMessage: string) {
     try {
-      await discord.editMessage(this.data, editedMessage);
+      await discord.editMessage(this.data, editedMessage)
     } catch (err) {
-      console.error(`Error editing message: ${err.message}`);
+      console.error(`Error editing message: ${err.message}`)
     }
   }
 }
 
-export class DiscordWorker<JobData> extends Worker<DiscordJob> {
+export class DiscordWorker<
+  DataType = unknown,
+  ResultType = unknown
+> extends Worker<DiscordJob> {
   constructor(
     name: string,
-    processor: (job) => Promise<void>,
+    processor: Processor<DataType, ResultType>,
     options?: WorkerOptions
   ) {
     super(
       name,
       async (job) => {
-        const { threadId } = job.data
-        const discordJob = new DiscordJob(job.queue, job.data, job.opts);
-        await processor(discordJob);
+        const discordJob = new DiscordJob(job.queue, job.data, job.opts)
+        await processor(discordJob)
       },
       {
         concurrency: 10, // default concurrency
