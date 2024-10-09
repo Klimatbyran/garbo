c35b69d865b7c83384a7c3653c70a680c51d8fc1
Christian Landgren <christian@landgren.nu>
2024-10-09 17:08:14 +0200

    Latest code
    Co-authored-by: Samuel Plumppu <Greenheart@users.noreply.github.com>

--- src/workers/downloadPDF.ts
+++ src/workers/downloadPDF.ts
@@ -1,11 +1,11 @@
-import { Worker, Job } from 'bullmq'
-import redis from '../config/redis'
+import { Job } from 'bullmq'
 import pdf from 'pdf-parse-debugging-disabled'
 import { splitText } from '../queues'
 import discord from '../discord'
 import * as crypto from 'crypto'
+import { DiscordWorker, DiscordWorkerJobData } from '../lib/DiscordWorker'
 
-class JobData extends Job {
+class JobData implements DiscordWorkerJobData {
   declare data: {
     url: string
     threadId: string
@@ -16,79 +16,71 @@ function hashPdf(pdfBuffer: Buffer): string {
   return crypto.createHash('sha256').update(pdfBuffer).digest('hex')
 }
 
-const worker = new Worker(
-  'downloadPDF',
-  async (job: JobData) => {
-    const { url } = job.data
+const worker = new DiscordWorker<JobData>('downloadPDF', async (job) => {
+  const { url } = job.data
 
-    job.log(`Downloading from url: ${url}`)
-    const message = await discord.sendMessage(job.data, `🤖 Laddar ner PDF...`)
+  job.log(`Downloading from url: ${url}`)
 
-    try {
-      const response = await fetch(url, {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36', //Garbo/1.0 (Linux; OpenAI 4;) Klimatkollen',
-        },
-      })
-      if (!response.ok) {
-        await discord.sendMessage(
-          job.data,
-          `Nedladdning misslyckades: ${response.statusText}`
-        )
-        throw new Error(`Nedladdning misslyckades: ${response.statusText}`)
-      }
-      message?.edit(`🤖 Tolkar PDF...`)
-
-      // save to disk
-      // const pdfPath = path.join(__dirname, 'temp.pdf')
-      // const pdfStream = fs.create
-      // const pdfStream = fs.create
-      // response.body.pipe(pdfStream)
-      // pdfStream.on('finish', () => {
-      //   console.log('pdf saved')
-      // })
-
-      const buffer = await response.arrayBuffer()
-
-      let doc
-      try {
-        doc = await pdf(buffer)
-      } catch (error) {
-        throw new Error('Error parsing PDF')
-      }
-      const text = doc.text
-      message.edit(`🤖 Hittade ${text.length} tecken. Delar upp i sidor...`)
-
-      let pdfHash = ''
-      try {
-        pdfHash = await hashPdf(Buffer.from(buffer))
-      } catch (error) {
-        job.log(`Error indexing PDF: ${error.message}`)
-      }
-      message.edit(`✅ PDF nedladdad!`)
-
-      splitText.add('split text ' + text.slice(0, 20), {
-        ...job.data,
-        url,
-        text,
-        pdfHash,
-      })
-
-      return doc.text
-    } catch (error) {
-      discord.sendMessage(
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36', //Garbo/1.0 (Linux; OpenAI 4;) Klimatkollen',
+      },
+    })
+    if (!response.ok) {
+      await discord.sendMessage(
         job.data,
-        `Fel vid nedladdning av PDF: ${error.message}`
+        `Nedladdning misslyckades: ${response.statusText}`
       )
-      job.log(`Error downloading PDF: ${error.message}`)
-      throw error
+      throw new Error(`Nedladdning misslyckades: ${response.statusText}`)
     }
-  },
-  {
-    connection: redis,
-    concurrency: 10,
+    message?.edit(`🤖 Tolkar PDF...`)
+
+    // save to disk
+    // const pdfPath = path.join(__dirname, 'temp.pdf')
+    // const pdfStream = fs.create
+    // const pdfStream = fs.create
+    // response.body.pipe(pdfStream)
+    // pdfStream.on('finish', () => {
+    //   console.log('pdf saved')
+    // })
+
+    const buffer = await response.arrayBuffer()
+
+    let doc
+    try {
+      doc = await pdf(buffer)
+    } catch (error) {
+      throw new Error('Error parsing PDF')
+    }
+    const text = doc.text
+    message.edit(`🤖 Hittade ${text.length} tecken. Delar upp i sidor...`)
+
+    let pdfHash = ''
+    try {
+      pdfHash = await hashPdf(Buffer.from(buffer))
+    } catch (error) {
+      job.log(`Error indexing PDF: ${error.message}`)
+    }
+    message.edit(`✅ PDF nedladdad!`)
+
+    splitText.add('split text ' + text.slice(0, 20), {
+      ...job.data,
+      url,
+      text,
+      pdfHash,
+    })
+
+    return doc.text
+  } catch (error) {
+    discord.sendMessage(
+      job.data,
+      `Fel vid nedladdning av PDF: ${error.message}`
+    )
+    job.log(`Error downloading PDF: ${error.message}`)
+    throw error
   }
-)
+})
 
 export default worker
