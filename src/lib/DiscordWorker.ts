8b66db74333c420522c480dafc83d631b22e1875
Christian Landgren <christian@landgren.nu>
2024-10-23 15:56:02 +0200

    feat: Add Discord job methods for message handling and thread naming

--- src/lib/DiscordWorker.ts
+++ src/lib/DiscordWorker.ts
@@ -11,6 +11,7 @@ export class DiscordJob extends Job {
     wikidataId?: string
     messageId?: string
   }
+
   message: any
   sendMessage: (
     msg: string | { content: string; components: any[] }
@@ -20,90 +21,83 @@ export class DiscordJob extends Job {
   getChildrenEntries: () => Promise<any>
 }
 
+export function addDiscordMethods(job: DiscordJob) {
+  let message = null
+  /**
+   * Combine results of children jobs into a single object.
+   */
+  job.getChildrenEntries = async () => {
+    return job
+      .getChildrenValues()
+      .then((values) => Object.values(values))
+      .then((values) =>
+        values
+          .map((value) => {
+            // Only parse the result for children jobs that returned potential JSON
+            if (value && typeof value === 'string') {
+              // NOTE: This still assumes all children jobs return JSON, and will crash if we return string results.
+              return Object.entries(JSON.parse(value))
+            } else {
+              return []
+            }
+          })
+          .flat()
+      )
+      .then((values) => Object.fromEntries(values))
+  }
+
+  job.sendMessage = async (msg) => {
+    message = await discord.sendMessage(job.data, msg)
+    if (!message) return undefined // TODO: throw error?
+    await job.updateData({ ...job.data, messageId: message.id })
+    return message
+  }
+
+  job.editMessage = (msg) => {
+    if (!message && job.data.messageId) {
+      const { channelId, threadId, messageId } = job.data
+      message = discord.findMessage({
+        channelId,
+        threadId,
+        messageId,
+      })
+    }
+    if (message && message.edit) {
+      try {
+        return message.edit(msg)
+      } catch (err) {
+        job.log(
+          'error editing Discord message:' +
+            err.message +
+            '\n' +
+            JSON.stringify(message)
+        )
+        return job.sendMessage(msg)
+      }
+    } else {
+      return job.sendMessage(msg)
+    }
+  }
+
+  job.setThreadName = async (name) => {
+    const thread = (await discord.client.channels.fetch(
+      job.data.threadId
+    )) as TextChannel
+    return thread.setName(name)
+  }
+  return job
+}
+
 export class DiscordWorker<T extends DiscordJob> extends Worker<any> {
   constructor(
     name: string,
-    callback: (job: T) => Promise<any>,
+    callback: (job: T) => any,
     options?: WorkerOptions
   ) {
-    super(
-      name,
-      async (job: T) => {
-        let message = null
-        /**
-         * Combine results of children jobs into a single object.
-         */
-        job.getChildrenEntries = async () => {
-          return job
-            .getChildrenValues()
-            .then((values) => Object.values(values))
-            .then((values) =>
-              values
-                .map((value) => {
-                  // Only parse the result for children jobs that returned potential JSON
-                  if (value && typeof value === 'string') {
-                    // NOTE: This still assumes all children jobs return JSON, and will crash if we return string results.
-                    return Object.entries(JSON.parse(value))
-                  } else {
-                    return []
-                  }
-                })
-                .flat()
-            )
-            .then((values) => Object.fromEntries(values))
-        }
-
-        job.sendMessage = async (msg) => {
-          message = await discord.sendMessage(job.data, msg)
-          if (!message) return undefined // TODO: throw error?
-          await job.updateData({ ...job.data, messageId: message.id })
-          return message
-        }
-
-        job.editMessage = (msg) => {
-          if (!message && job.data.messageId) {
-            const { channelId, threadId, messageId } = job.data
-            message = discord.findMessage({
-              channelId,
-              threadId,
-              messageId,
-            })
-          }
-          if (message) {
-            try {
-              return message.edit(msg)
-            } catch (err) {
-              job.log('error editing Discord message:' + err.message)
-              return job.sendMessage(msg)
-            }
-          } else {
-            return job.sendMessage(msg)
-          }
-        }
-
-        job.setThreadName = async (name) => {
-          const thread = (await discord.client.channels.fetch(
-            job.data.threadId
-          )) as TextChannel
-          return thread.setName(name)
-        }
-
-        try {
-          const values = await job.getChildrenEntries()
-          if (values && Object.keys(values).length) {
-            await job.updateData({ ...job.data, childrenValues: values })
-          }
-          return callback(job)
-        } catch (err) {
-          job.sendMessage(`❌ ${this.name}: ${err.message}`)
-          throw err
-        }
-      },
-      {
-        connection: redis,
-        concurrency: 10,
-        ...options,
-      }
-    )
+    super(name, callback, {
+      connection: redis,
+      concurrency: 10,
+      ...options,
+    })
   }
 }
