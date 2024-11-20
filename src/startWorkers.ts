12f1fa9ebef380306614f7bd41218cacdba91ab7
Christian Landgren <christian@landgren.nu>
2024-11-20 09:14:18 +0100

    chore: log the env variables for troubleshooting

--- src/startWorkers.ts
+++ src/startWorkers.ts
@@ -3,9 +3,11 @@ import discord from './discord'
 import { workers } from './workers'
 
 console.log('Starting workers...')
+
 Promise.all(workers.map((worker) => worker.run()))
   .then((results) => results.join('\n'))
   .then(console.log)
 
 discord.login()
 console.log('Discord bot started')
+console.log('env', process.env)
