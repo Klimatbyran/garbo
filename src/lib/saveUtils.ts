8be2df571712aeb0e5f2bf73f68407993981ce9d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-12-05 09:58:48 +0100

    Omit metadata when diffing
    Co-authored-by: Hugo Björk <hugo.bjork@netlight.com>

--- src/lib/saveUtils.ts
+++ src/lib/saveUtils.ts
@@ -31,8 +31,29 @@ export const defaultMetadata = (url: string) => ({
   comment: 'Parsed by Garbo AI',
 })
 
-const omit = (obj: any, keys: string[]) =>
-  Object.fromEntries(Object.entries(obj).filter(([key]) => !keys.includes(key)))
+/**
+ * Recusrively remove the provided keys from all levels of the object.
+ *
+ * Handles circular references.
+ * Source: https://stackoverflow.com/a/72493889
+ */
+const recursiveOmit = <T extends Object>(
+  obj: T,
+  keys: Set<string>,
+  visitedIn?: Set<any>
+): T => {
+  if (obj == null || typeof obj !== 'object') return obj
+  const visited = visitedIn ?? new Set()
+  visited.add(obj)
+  Object.entries(obj).forEach(([key, val]) => {
+    if (keys.has(key)) {
+      delete obj[key as keyof T]
+    } else if (typeof val === 'object' && !visited.has(val)) {
+      recursiveOmit(val, keys, visited)
+    }
+  })
+  return obj
+}
 
 export const askDiff = async (before: any, after: any) => {
   if (!before || !after) return 'NO_CHANGES'
@@ -44,8 +65,8 @@ Focus only on the actual values that have changed.
 When handling years and ambiguous dates, always use the last year in the period (e.g. startDate: 2020 - endDate: 2021 should be referred to as 2021).
 NEVER REPEAT UNCHANGED VALUES OR UNCHANGED YEARS! If nothing important has changed, just write "NO_CHANGES".`,
     JSON.stringify({
-      before: omit(before, ['metadata']),
-      after: omit(after, ['metadata']),
+      before: recursiveOmit(structuredClone(before), new Set(['metadata'])),
+      after: recursiveOmit(structuredClone(after), new Set(['metadata'])),
     })
   )
 }
