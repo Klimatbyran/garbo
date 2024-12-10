e960dc36afd59c209dd46fa4a459b69cdb6bb3d9
Christian Landgren <christian@landgren.nu>
2024-12-10 10:18:11 +0100

    fix: dotenv-cli and remove duplicate code

--- src/config/chromadb.ts
+++ src/config/chromadb.ts
@@ -17,22 +17,3 @@ export default {
       }
     : undefined,
 }
-import 'dotenv/config'
-import { z } from 'zod'
-
-const envSchema = z.object({
-  CHROMA_HOST: z.string().default('http://127.0.0.1:8000'),
-  CHROMA_TOKEN: z.string().optional(),
-})
-
-const env = envSchema.parse(process.env)
-
-export default {
-  path: env.CHROMA_HOST,
-  auth: env.CHROMA_TOKEN
-    ? {
-        provider: 'token',
-        credentials: env.CHROMA_TOKEN,
-      }
-    : undefined,
-}
