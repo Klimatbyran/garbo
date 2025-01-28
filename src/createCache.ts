8cd9ed9efa41981f508abcb9fc88fd698e72c964
Hugo Björk <hugo.bjork@netlight.com>
2025-01-28 09:58:41 +0100

    fixed redis prefix to distinguish bull vs redis cache

--- src/createCache.ts
+++ src/createCache.ts
@@ -1,28 +1,33 @@
 import { createClient } from 'redis'
 import redisConfig from './config/redis'
 
+// Note: This prefix is a good seperator from the bullMQ jobs
+export const API_REDIS_PREFIX = 'redis_api/'
+
 const redis = createClient({ ...redisConfig })
 redis.connect()
 
 export function createServerCache({ maxAge }: { maxAge: number }) {
   return {
     async set(key: string, value: any) {
-      await redis.set(key, JSON.stringify(value), {
-        PX: maxAge,
-      })
+      const namespacedKey = `${API_REDIS_PREFIX}${key}`
+      await redis.set(namespacedKey, JSON.stringify(value), { PX: maxAge })
     },
     async get(key: string) {
-      const cached = await redis.get(key)
+      const namespacedKey = `${API_REDIS_PREFIX}${key}`
+      const cached = await redis.get(namespacedKey)
       return cached ? JSON.parse(cached) : null
     },
     async has(key: string) {
-      return (await redis.exists(key)) === 1
+      const namespacedKey = `${API_REDIS_PREFIX}${key}`
+      return (await redis.exists(namespacedKey)) === 1
     },
     async delete(key: string) {
-      await redis.del(key)
+      const namespacedKey = `${API_REDIS_PREFIX}${key}`
+      await redis.del(namespacedKey)
     },
     async clear() {
-      const keys = await redis.keys('*')
+      const keys = await redis.keys(`${API_REDIS_PREFIX}*`)
       if (keys.length > 0) await redis.del(keys)
     },
   }
