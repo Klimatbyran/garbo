8dacaee9703ebef75098cd734aade3cd4a1e0540
Moritz Zingg <moritz.zingg@netlight.com>
2025-02-13 11:32:08 +0100

    Added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import "eslint-plugin-only-warn";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
