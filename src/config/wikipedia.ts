d9044465a51b0435e8a1d2fcd6185d1c2364a8b4
Oliver Kiss <okis@netlight.com>
2025-03-12 16:58:16 +0100

    [FIX] Incorrectly formatted emissions data when generating article text (#667)
    * Fixed incorrectly formatted emissions data when generating article text
    
    * Login only when performing updates, read env vars from wikipedia config file

--- src/config/wikipedia.ts
+++ src/config/wikipedia.ts
@@ -1,5 +1,19 @@
-export const WIKI_LANGUAGE = "sv"
-export const WIKI_API_URL = "test.wikipedia.org"
-export const EDIT_MSG_SUMMARY = "Bot: Update emissions data"
-export const REPORT_REFERENCE_NAME = "klimatkollen-emissions-report-reference"
-export const KLIMATOLLEN_REFERENCE_NAME = "klimatkollen-emissions-reference"
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  WIKI_USERNAME: z.string(),
+  WIKI_PASSWORD: z.string(),
+})
+
+const env = envSchema.parse(process.env)
+
+export default {
+    language: "sv",
+    apiUrl: "test.wikipedia.org",
+    editMsgSummary: "Bot: Update emissions data",
+    reportReferenceName: "klimatkollen-emissions-report-reference",
+    klimtkollenReferenceName: "klimatkollen-emissions-reference",
+    wikiUsername: env.WIKI_USERNAME,
+    wikiPassword: env.WIKI_PASSWORD
+}
