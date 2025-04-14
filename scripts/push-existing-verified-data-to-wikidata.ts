557396b7d3c9f59e4bb4b1c821e174bf48c8d7d4
Moritz Zingg <moritz.zingg@netlight.com>
2025-04-14 16:55:00 +0200

    Added update of total emissions to wikidata upload script

--- scripts/push-existing-verified-data-to-wikidata.ts
+++ scripts/push-existing-verified-data-to-wikidata.ts
@@ -1,10 +1,30 @@
 import { PrismaClient } from '@prisma/client'
 import wikidataConfig from "../src/config/wikidata";
-import { bulkCreateOrEditCarbonFootprintClaim, Claim } from '../src/lib/wikidata';
+import { bulkCreateOrEditCarbonFootprintClaim, Claim, diffTotalCarbonFootprintClaims, editEntity, getClaims } from '../src/lib/wikidata';
+import { exit } from 'process';
 
 //Currently still in testing the filters filter out only data related to ABB as this company is present in the Sandbox
 const entityDownloadId: `Q${number}` = "Q731938";
 const entityUploadId: `Q${number}` = "Q238689";
+
+/*const entityIds: `Q${number}`[] = ["Q130367306", "Q7315107", "Q10674550", 
+    "Q10397786", "Q671398", "Q10461247", "Q106626934", "Q10719187", 
+    "Q10720019", "Q30289762", "Q17102820", "Q109790098", "Q130367275", 
+    "Q98602838", "Q3429427", "Q738421", "Q106564093", "Q10429829", 
+    "Q10590357", "Q975655", "Q18287127", "Q130366922", "Q5318875", 
+    "Q1657823", "Q115167526", "Q109780258", "Q10397672", "Q115112945", 
+    "Q111843935", "Q891345", "Q65083539", "Q47498532", "Q1275733", 
+    "Q10494668", "Q1337240", "Q10600264", "Q52912", "Q16427839", 
+    "Q65196792", "Q1324884", "Q130366928", "Q10599821", "Q215293", 
+    "Q24882789", "Q51581693", "Q10651354"
+];
+
+for(const entityId of entityIds) {
+    const {newClaims, rmClaims} = await diffTotalCarbonFootprintClaims([], await getClaims(entityId), []);
+    console.log(newClaims);
+    await editEntity(entityId, newClaims, rmClaims);
+}
+exit(0);*/
 
 const prisma = new PrismaClient()
 
