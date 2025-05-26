bd0753edfb3213ca269b33a612b86988d4ad4c5c
Moritz Zingg <moritz.zingg@netlight.com>
2025-05-26 15:04:19 +0200

    Added gleif api to script

--- scripts/push-lei-number.ts
+++ scripts/push-lei-number.ts
@@ -1,4 +1,9 @@
-import { getLEINumber } from '../src/lib/wikidata';
+import { getLEINumberClaim } from '../src/lib/wikidata';
+import { getLEINumbers } from '../src/lib/gleif';
+import { leiPrompt, leiSchema } from '../src/prompts/lei';
+import { ask } from '../src/lib/openai';
+import { zodResponseFormat } from 'openai/helpers/zod';
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
 const env = 'http://localhost:3000/api';
 const secret = '123F';
@@ -18,6 +23,44 @@ async function getApiToken(user: string) {
   return (await response.json()).token
 }
 
+async function getLEINumberFromGleif(companyName: string): Promise<string | undefined> {
+  const searchResults = await getLEINumbers(companyName);
+
+  if (!searchResults || searchResults.length === 0) {
+    console.log(searchResults);
+    return;
+  }
+
+  const response = await ask(
+    [
+      {
+        role: 'system',
+        content: `I have a company named ${companyName} and I am looking for the wikidata entry related to this company. Be helpful and try to be accurate.`,
+      },
+      { role: 'user', content: leiPrompt },
+      {
+        role: 'assistant',
+        content: 'OK. Just send me the wikidata search results?',
+      },
+      {
+        role: 'user',
+        content: JSON.stringify(searchResults, null, 2),
+      },
+    ].filter((m) => m && m.content?.length > 0) as ChatCompletionMessageParam[],
+    {response_format: zodResponseFormat(leiSchema, 'lei')}
+  )
+
+  const { success, error, data } = leiSchema.safeParse(
+    JSON.parse(response)
+  )
+
+  if (error || !success) {
+    throw new Error('Failed to parse ' + error.message)
+  }
+
+  return data.lei;
+}
+
 async function updateLEI(wikidataId: string, token: string, lei: string) {
   const response = await fetch(
     `${env}/companies/${wikidataId}`,
@@ -33,7 +76,7 @@ async function updateLEI(wikidataId: string, token: string, lei: string) {
   return response;
 }
 
-async function getCompanies(): Promise<{lei: string, wikidataId: string}[]> {
+async function getCompanies(): Promise<{lei: string, wikidataId: string, name: string}[]> {
   const response = await fetch(`${env}/companies`);
   return (await response.json());
 }
@@ -46,7 +89,7 @@ async function pushLeiNumbers(): Promise<string[]> {
   for(const company of companies) {
     if(!company.lei) {
       console.log(`fetching LEI for ${company.wikidataId}`)
-      const lei = await getLEINumber(company.wikidataId as `Q${number}`);
+      let lei = (await getLEINumberClaim(company.wikidataId as `Q${number}`)) ?? (await getLEINumberFromGleif(company.name));
       if(lei) {
         await updateLEI(company.wikidataId, token, lei);
         updatedCompanies.push(company.wikidataId);
