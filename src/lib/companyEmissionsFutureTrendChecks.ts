51213dd5a676e699597662be15a22c90f479297b
elvbom <ecdb@protonmail.com>
2025-09-22 20:32:50 +0200

    move checks to new file

---
+++ src/lib/companyEmissionsFutureTrendChecks.ts
@@ -0,0 +1,58 @@
+import {
+  has3YearsOfNonNullData,
+  ReportedPeriod,
+} from './companyEmissionsFutureTrendCalculator'
+
+export function checkDataReportedForBaseYear(
+  reportedPeriods: ReportedPeriod[],
+  baseYear: number,
+) {
+  return reportedPeriods.some((period) => period.year === baseYear)
+}
+
+export function checkForNulls(emission: number | null | undefined) {
+  return emission !== null && emission !== undefined
+}
+
+export function checkDataReportedFor3YearsAfterBaseYear(
+  reportedPeriods: ReportedPeriod[],
+  baseYear: number,
+) {
+  return reportedPeriods.slice(baseYear).length >= 3
+}
+
+export function filterFromBaseYear(
+  reportedPeriods: ReportedPeriod[],
+  baseYear: number,
+) {
+  return reportedPeriods.filter((period) => period.year >= baseYear)
+}
+
+export function checkScope3DataFor3YearsAfterBaseYear(
+  reportedPeriods: ReportedPeriod[],
+  baseYear: number,
+) {
+  const periodsAfterBaseYear = filterFromBaseYear(reportedPeriods, baseYear)
+  return has3YearsOfNonNullData(periodsAfterBaseYear, 'scope3')
+}
+
+export function checkOnlyScope1And2DataFor3YearsAfterBaseYear(
+  reportedPeriods: ReportedPeriod[],
+  baseYear: number,
+) {
+  const periodsAfterBaseYear = filterFromBaseYear(reportedPeriods, baseYear)
+  return (
+    periodsAfterBaseYear.every((period) => !period.emissions.scope3) &&
+    has3YearsOfNonNullData(periodsAfterBaseYear, 'scope1and2')
+  )
+}
+
+export function checkScope3DataFor3Years(reportedPeriods: ReportedPeriod[]) {
+  return has3YearsOfNonNullData(reportedPeriods, 'scope3')
+}
+
+export function checkScope1And2DataFor3Years(
+  reportedPeriods: ReportedPeriod[],
+) {
+  return has3YearsOfNonNullData(reportedPeriods, 'scope1and2')
+}
