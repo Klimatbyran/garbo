43013a6a50f39cca2e1fc12f3705232c31801cf7
elvbom <ecdb@protonmail.com>
2025-09-24 17:36:22 +0200

    rename calc test and test data, add more test data and move check test to own file

---
+++ tests/companyEmissionsFutureTrendCalculator.spec.ts
@@ -0,0 +1,125 @@
+import {
+  calculateLADTrendSlope,
+  extractEmissionsArray,
+  extractScope1And2EmissionsArray,
+  extractScope3EmissionsArray,
+  has3YearsOfNonNullData,
+  has3YearsOfReportedData,
+  calculateFututreEmissionTrend,
+} from '../src/lib/companyEmissionsFutureTrendCalculator'
+import {
+  reportedPeriods,
+  reportingPeriodsWithMixedScopeData,
+  scope3EmissionsArray,
+  scope1And2EmissionsArray,
+  scope1and2TotalEmissionsArray,
+  aleEmissionSlope,
+  aleEmissionsArray,
+  sveviaEmissions,
+  sveviaEmissionsArray,
+  sveviaEmissionSlope,
+} from './companyEmissionsFutureTrendCalculatorTestData'
+
+describe('Company Emissions Calculator', () => {
+  describe('calculateFututreEmissionTrend', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    afterEach(() => {
+      jest.restoreAllMocks()
+    })
+
+    // test has3YearsOfReportedData
+    test('should return true if there is sufficient emissions data', () => {
+      const result = has3YearsOfReportedData(reportedPeriods)
+      expect(result).toEqual(true)
+    })
+
+    test('should return false if there is not sufficient emissions data', () => {
+      const result = has3YearsOfReportedData(reportedPeriods.slice(0, 2))
+      expect(result).toEqual(false)
+    })
+
+    // test has3YearsOfNonNullData
+    test('should return true if there is at least 3 years of non null data', () => {
+      const result = has3YearsOfNonNullData(reportedPeriods, 'scope3')
+      expect(result).toEqual(true)
+    })
+
+    test('should return false if there is less than 3 years of non null data', () => {
+      const result = has3YearsOfNonNullData(
+        reportedPeriods.slice(6, 9),
+        'scope3',
+      )
+      expect(result).toEqual(false)
+    })
+
+    // test extractEmissionsArray
+    test('should return expected result for total emissions array', () => {
+      const result = extractEmissionsArray(reportedPeriods, 'scope1and2')
+      expect(result).toEqual(scope1and2TotalEmissionsArray)
+    })
+
+    // test extractScope3EmissionsArray
+    test('should return expected result for scope 3 emissions array', () => {
+      const result = extractScope3EmissionsArray(reportedPeriods)
+      expect(result).toEqual(scope3EmissionsArray)
+    })
+
+    // test extractScope1And2EmissionsArray
+    test('should return expected result for scope 1 and 2 emissions array', () => {
+      const result = extractScope1And2EmissionsArray(
+        reportingPeriodsWithMixedScopeData.slice(1, 4),
+      )
+      expect(result).toEqual(scope1And2EmissionsArray)
+    })
+
+    // test extractEmissionsArray
+    test('should return expected result for scope 3 emissions array', () => {
+      const result = extractEmissionsArray(reportedPeriods, 'scope3')
+
+      expect(result).toEqual(scope3EmissionsArray)
+    })
+
+    test('should return expected result for scope 1 and 2 emissions array', () => {
+      const result = extractEmissionsArray(
+        reportingPeriodsWithMixedScopeData.slice(1, 4),
+        'scope1and2',
+      )
+      expect(result).toEqual(scope1And2EmissionsArray)
+    })
+
+    // Test for LAD slope per index step
+    test('should return expected result for LAD slope per index step', () => {
+      const result = calculateLADTrendSlope(scope1and2TotalEmissionsArray)
+
+      const expectedResult = 2.8
+      const roundedResult = Number(result.toFixed(2))
+      expect(roundedResult).toEqual(expectedResult)
+    })
+
+    test('should return expected result for LAD slope per index step for ale to ensure model performs same as for municipalities', () => {
+      const result = calculateLADTrendSlope(aleEmissionsArray)
+      const roundedResult = Number(result.toFixed(4))
+      expect(roundedResult).toEqual(aleEmissionSlope)
+    })
+
+    test('should return expected result for LAD slope per index step for svevia', () => {
+      const result = calculateLADTrendSlope(sveviaEmissionsArray)
+      const roundedResult = Number(result.toFixed(4))
+      expect(roundedResult).toEqual(sveviaEmissionSlope)
+    })
+
+    // test calculateFututreEmissionTrend
+    test('should return expected result for calculateFututreEmissionTrend', () => {
+      const result = calculateFututreEmissionTrend({
+        reportedPeriods: sveviaEmissions,
+      })
+
+      // should use total calculated emissions 2020-2024
+      const roundedResult = Number(result.futureEmissionTrendSlope?.toFixed(4))
+      expect(roundedResult).toEqual(sveviaEmissionSlope)
+    })
+  })
+})
---
+++ tests/companyEmissionsFutureTrendCalculatorTestData.ts
@@ -0,0 +1,334 @@
+import { ReportedPeriod } from '../src/lib/companyEmissionsFutureTrendCalculator'
+
+export const reportedPeriods = [
+  {
+    year: 1,
+    emissions: {
+      calculatedTotalEmissions: 7,
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 6,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 2,
+    emissions: {
+      calculatedTotalEmissions: 14,
+      scope1: {
+        total: 1,
+      },
+      scope2: {
+        mb: 1,
+        lb: 1,
+        unknown: 0,
+      },
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 12,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 3,
+    emissions: {
+      calculatedTotalEmissions: 10,
+      scope1: {
+        total: 1,
+      },
+      scope2: {
+        mb: 1,
+        lb: 1,
+        unknown: 0,
+      },
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 8,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 4,
+    emissions: {
+      calculatedTotalEmissions: 17,
+      scope1: {
+        total: 2,
+      },
+      scope2: {
+        mb: 1,
+        lb: 1,
+        unknown: 0,
+      },
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 14,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 5,
+    emissions: {
+      calculatedTotalEmissions: 15,
+      scope1: {
+        total: 2,
+      },
+      scope2: {
+        mb: 1,
+        lb: 1,
+        unknown: 0,
+      },
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 12,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 6,
+    emissions: {
+      calculatedTotalEmissions: 21,
+      scope1: {
+        total: 1,
+      },
+      scope2: {
+        mb: 2,
+        lb: 2,
+        unknown: 0,
+      },
+      scope3: {
+        categories: [
+          {
+            category: 1,
+            total: 18,
+          },
+        ],
+      },
+    },
+  },
+  {
+    year: 7,
+    emissions: {
+      calculatedTotalEmissions: 26,
+    },
+  },
+  {
+    year: 8,
+    emissions: {
+      calculatedTotalEmissions: 23,
+    },
+  },
+]
+
+export const reportingPeriodsWithMixedScopeData = [
+  {
+    year: 1,
+    emissions: {
+      calculatedTotalEmissions: 3,
+      scope1: { total: 1 },
+      scope2: { mb: 1, lb: 1, unknown: 0 },
+      scope3: { categories: [{ category: 1, total: 1 }] },
+    },
+  },
+  {
+    year: 2,
+    emissions: {
+      calculatedTotalEmissions: 4,
+      scope1: { total: 2 },
+      scope2: { mb: 2, lb: 2, unknown: 0 },
+    },
+  },
+  {
+    year: 3,
+    emissions: {
+      calculatedTotalEmissions: 6,
+      scope1: { total: 3 },
+      scope2: { mb: 3, lb: 3, unknown: 0 },
+    },
+  },
+  {
+    year: 4,
+    emissions: {
+      calculatedTotalEmissions: 8,
+      scope1: { total: 4 },
+      scope2: { mb: 4, lb: 4, unknown: 0 },
+    },
+  },
+]
+
+export const scope3EmissionsArray = [
+  { year: 1, emissions: 7 },
+  { year: 2, emissions: 14 },
+  { year: 3, emissions: 10 },
+  { year: 4, emissions: 17 },
+  { year: 5, emissions: 15 },
+  { year: 6, emissions: 21 },
+]
+
+export const scope1And2EmissionsArray = [
+  { year: 2, emissions: 4 },
+  { year: 3, emissions: 6 },
+  { year: 4, emissions: 8 },
+]
+
+export const scope1and2TotalEmissionsArray = [
+  { year: 1, emissions: 7 },
+  { year: 2, emissions: 14 },
+  { year: 3, emissions: 10 },
+  { year: 4, emissions: 17 },
+  { year: 5, emissions: 15 },
+  { year: 6, emissions: 21 },
+  { year: 7, emissions: 26 },
+  { year: 8, emissions: 23 },
+]
+
+// municipality emissions data to compare model result with municipality model in python
+export const aleEmissionsArray = [
+  { year: 2015, emissions: 153825.085295 },
+  { year: 2016, emissions: 152178.7955 },
+  { year: 2017, emissions: 155980.925217 },
+  { year: 2018, emissions: 162008.760113 },
+  { year: 2019, emissions: 168504.59322 },
+  { year: 2020, emissions: 151960.022899 },
+  { year: 2021, emissions: 157751.287931 },
+  { year: 2022, emissions: 142529.055614 },
+  { year: 2023, emissions: 136223.552398 },
+]
+
+export const aleEmissionSlope = -1340.3008
+
+// company data for Svevia
+export const sveviaEmissions: ReportedPeriod[] = [
+  {
+    year: 2024,
+    emissions: {
+      calculatedTotalEmissions: 38645,
+      scope1: { total: 29149 },
+      scope2: { mb: 4719, lb: 798, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 4777,
+        statedTotalEmissions: { total: 4777 },
+        categories: [
+          { category: 5, total: null },
+          { category: 6, total: null },
+        ],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2023,
+    emissions: {
+      calculatedTotalEmissions: 31078,
+      scope1: { total: 22741 },
+      scope2: { mb: 3315, lb: 800, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 5022,
+        statedTotalEmissions: { total: 5022 },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2022,
+    emissions: {
+      calculatedTotalEmissions: 36972,
+      scope1: { total: 28163 },
+      scope2: { mb: 3982, lb: 683, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 4827,
+        statedTotalEmissions: { total: 4827 },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2021,
+    emissions: {
+      calculatedTotalEmissions: 29395,
+      scope1: { total: 24876 },
+      scope2: { mb: 3530, lb: 706, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 989,
+        statedTotalEmissions: { total: 989 },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2020,
+    emissions: {
+      calculatedTotalEmissions: 27964,
+      scope1: { total: 24103 },
+      scope2: { mb: 3626, lb: 958, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 235,
+        statedTotalEmissions: { total: 235 },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2019,
+    emissions: {
+      calculatedTotalEmissions: 25084,
+      scope1: { total: 25084 },
+      scope2: null,
+      scope3: {
+        calculatedTotalEmissions: 235,
+        statedTotalEmissions: { total: 235 },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+  {
+    year: 2015,
+    emissions: {
+      calculatedTotalEmissions: 37008,
+      scope1: { total: 32395 },
+      scope2: { mb: 4613, lb: 1007, unknown: null },
+      scope3: {
+        calculatedTotalEmissions: 0,
+        statedTotalEmissions: { total: null },
+        categories: [],
+      },
+      statedTotalEmissions: null,
+    },
+  },
+]
+
+export const sveviaEmissionsArray = [
+  { year: 2020, emissions: 27964 },
+  { year: 2021, emissions: 29395 },
+  { year: 2022, emissions: 36972 },
+  { year: 2023, emissions: 31078 },
+  { year: 2024, emissions: 38645 },
+]
+
+export const sveviaEmissionSlope = 2670.25
+
+export const sveviaLastPeriod = sveviaEmissions[sveviaEmissions.length - 1]
