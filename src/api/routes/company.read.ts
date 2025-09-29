fa6b576a4128071b0f19c0c6b06bf5b86b1bd798
elvbom <ecdb@protonmail.com>
2025-09-29 17:01:36 +0200

    restore company read route to main version

--- src/api/routes/company.read.ts
+++ src/api/routes/company.read.ts
@@ -15,6 +15,7 @@ import {
 } from '../schemas'
 import { redisCache } from '../..'
 
+
 export async function companyReadRoutes(app: FastifyInstance) {
   app.register(cachePlugin)
 
@@ -29,7 +30,6 @@ export async function companyReadRoutes(app: FastifyInstance) {
 
         response: {
           200: CompanyList,
-          304: {},
         },
       },
     },
@@ -65,7 +65,7 @@ export async function companyReadRoutes(app: FastifyInstance) {
       reply.header('ETag', `${currentEtag}`)
 
       reply.send(companies)
-    },
+    }
   )
 
   app.get(
@@ -94,12 +94,14 @@ export async function companyReadRoutes(app: FastifyInstance) {
               ...company.industry,
               industryGics: {
                 ...company.industry.industryGics,
-                ...getGics(company.industry.industryGics.subIndustryCode),
+                ...getGics(
+                  company.industry.industryGics.subIndustryCode
+                ),
               },
             }
           : null,
       })
-    },
+    }
   )
 
   app.get(
@@ -112,18 +114,15 @@ export async function companyReadRoutes(app: FastifyInstance) {
         tags: getTags('Companies'),
         querystring: companySearchQuerySchema,
         response: {
-          200: CompanyList,
+          200: CompanyList
         },
       },
     },
-    async (
-      request: FastifyRequest<{ Querystring: CompanySearchQuery }>,
-      reply,
-    ) => {
+    async (request: FastifyRequest<{ Querystring: CompanySearchQuery }>, reply) => {
       const { q } = request.query
       const companies = await companyService.getAllCompaniesBySearchTerm(q)
-      console.log(companies)
+      console.log(companies);
       reply.send(companies)
-    },
+    }
   )
 }
