fd16bf694d920abe5391b2b99c8acef1545cb98e
Dennis Moradkhani <demo@netlight.com>
2025-10-03 13:32:03 +0200

    use ask method instead of askStream

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,23 +1,36 @@
 import { assert } from 'console'
 import OpenAI from 'openai'
-import { ChatCompletionAssistantMessageParam, ChatCompletionChunk, ChatCompletionCreateParamsNonStreaming, ChatCompletionCreateParamsStreaming, ChatCompletionMessageParam, ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam, ResponseFormatJSONSchema } from 'openai/resources'
+import {
+  ChatCompletionCreateParamsNonStreaming,
+  ChatCompletionMessageParam,
+  ResponseFormatJSONSchema,
+} from 'openai/resources'
 import openaiConfig from '../config/openai'
-import { Stream } from 'openai/streaming'
 import { RequestOptions } from 'openai/core'
 
 const openai = new OpenAI({
   apiKey: openaiConfig.apiKey,
 })
 
-
-const ask = async (messages: ChatCompletionMessageParam[], options?: RequestOptions & {response_format?: ResponseFormatJSONSchema}) => {
-  const response = await openai.chat.completions.create({
+const ask = async (
+  messages: ChatCompletionMessageParam[],
+  options?: RequestOptions & {
+    onParagraph?: (response: string, paragraph: string) => void
+  } & { response_format?: ResponseFormatJSONSchema },
+) => {
+  const config = {
     messages: messages.filter((m) => m.content),
-    model: 'gpt-4o',
+    model: 'gpt-4o-2024-08-06',
     temperature: 0.1,
+    max_tokens: 4096,
+    response_format: options?.response_format,
     stream: false,
     ...options,
-  } as ChatCompletionCreateParamsNonStreaming)
+  } satisfies ChatCompletionCreateParamsNonStreaming
+
+  const response = await openai.chat.completions.create(
+    config as ChatCompletionCreateParamsNonStreaming,
+  )
 
   return response.choices[0].message.content ?? ''
 }
@@ -36,43 +49,4 @@ const askPrompt = async (prompt: string, context: string) => {
   ])
 }
 
-const askStream = async (
-  messages: (ChatCompletionSystemMessageParam
-    | ChatCompletionUserMessageParam
-    | ChatCompletionAssistantMessageParam)[],
-  options: RequestOptions  & {onParagraph?: (response: string, paragraph: string) => void} & {response_format?: ResponseFormatJSONSchema}
-) => {
-  const { stream: _, ...safeOpenAIOptions } = options
-
-  const config = {
-    messages: messages.filter((m) => m.content),
-    model: 'gpt-4o-2024-08-06',
-    temperature: 0.1,
-    stream: true,
-    max_tokens: 4096,
-    response_format: options.response_format,
-    ...safeOpenAIOptions,
-  } satisfies ChatCompletionCreateParamsStreaming;
-
-  const stream:  Stream<ChatCompletionChunk> = await openai.chat.completions.create(config);
-
-
-  let response = ''
-  let paragraph = ''
-  for await (const part of stream) {
-    const chunk = part.choices[0]?.delta?.content || ''
-    response += chunk
-    paragraph += chunk
-    if (options?.onParagraph && chunk.includes('\n')) {
-      options?.onParagraph(response, paragraph)
-      paragraph = ''
-    }
-  }
-  // send the rest if there is any
-  if (options?.onParagraph && paragraph)
-    options?.onParagraph(response, paragraph)
-
-  return response
-}
-
-export { openai, ask, askPrompt, askStream }
+export { openai, ask, askPrompt }
