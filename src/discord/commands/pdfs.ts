9f2fac58ffdadb342ebfe403aefe108b8720427d
Dennis Moradkhani <demo@netlight.com>
2025-10-15 11:27:56 +0200

    deprecate nlm parser and switch to docling as only pdf parser

--- src/discord/commands/pdfs.ts
+++ src/discord/commands/pdfs.ts
@@ -3,40 +3,41 @@ import {
   SlashCommandBuilder,
   TextChannel,
 } from 'discord.js'
-import { queues } from '../../queues';
+import { queues } from '../../queues'
 import { DiscordJob } from '../../lib/DiscordWorker'
 
 export default {
   data: new SlashCommandBuilder()
     .setName('pdfs')
     .setDescription(
-      'Skicka in en eller flera årsredovisningar och få tillbaka utsläppsdata.'
+      'Skicka in en eller flera årsredovisningar och få tillbaka utsläppsdata.',
     )
     .addStringOption((option) =>
       option
         .setName('urls')
         .setDescription('URLs to PDF files. Separate with comma or new lines.')
-        .setRequired(true)
+        .setRequired(true),
     )
-    .addBooleanOption((option) => 
+    .addBooleanOption((option) =>
       option
         .setName('auto-approve')
         .setDescription('Automatically approve the extracted data.')
-        .setRequired(false)
+        .setRequired(false),
     ),
 
   async execute(interaction: ChatInputCommandInteraction) {
     try {
       await interaction.deferReply({ ephemeral: true })
-      
+
       const urls = interaction.options
         .getString('urls')
         ?.split(/\s*,\s*|\s+/)
         .map((url) => url.trim()) // Remove whitespace
         .filter(Boolean) // Remove empty strings
         .filter((url) => url.startsWith('http')) // Only allow URLs
-      
-      const autoApprove = interaction.options.getBoolean('auto-approve') || false
+
+      const autoApprove =
+        interaction.options.getBoolean('auto-approve') || false
 
       if (!urls || !urls.length) {
         await interaction.followUp({
@@ -62,8 +63,10 @@ export default {
         })
 
         thread.send(`PDF i kö: ${url}`)
-        thread.send(`Be användaren att verifiera data: ${autoApprove ? 'Nej' : 'Ja'}`)
-        queues.nlmParsePDF.add(
+        thread.send(
+          `Be användaren att verifiera data: ${autoApprove ? 'Nej' : 'Ja'}`,
+        )
+        queues.parsePdf.add(
           'download ' + url.slice(-20),
           {
             url: url.trim(),
@@ -76,7 +79,7 @@ export default {
               delay: 60_000,
             },
             attempts: 10,
-          }
+          },
         )
       })
     } catch (error) {
