0ff32ae8b351b8dc6a137eaa5001d349ef7c2deb
Carl <carlwiren90@gmail.com>
2026-03-25 15:22:59 +0100

    Swapped name on file and added primary caching for registry

--- src/api/routes/internal/registry.read.ts
+++ src/api/routes/internal/registry.read.ts
@@ -2,7 +2,9 @@ import { FastifyInstance } from 'fastify'
 import { getTags } from '../../../config/openapi'
 import { cachePlugin } from '../../plugins/cache'
 import { RegistryList } from '../../schemas'
-import { registryService } from '@/api/services/registryServices'
+import { registryService } from '@/api/services/registryService'
+import { redisCache } from '@/index'
+import { prisma } from '@/lib/prisma'
 
 export async function registryReadRoutes(app: FastifyInstance) {
   app.register(cachePlugin)
@@ -21,7 +23,29 @@ export async function registryReadRoutes(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      const registry = await registryService.getReportRegistry()
+      const cacheKey = 'registry:etag'
+      let currentEtag: string = await redisCache.get(cacheKey)
+
+      const [registryCount] = await prisma.$transaction([prisma.report.count()])
+
+      // Create a unique fingerprint based on registry data
+      const databaseFingerprint = [registryCount].join('|')
+
+      if (!currentEtag || !currentEtag.startsWith(databaseFingerprint)) {
+        currentEtag = `${databaseFingerprint}-${new Date().toISOString()}`
+        await redisCache.set(cacheKey, JSON.stringify(currentEtag))
+      }
+
+      const dataCacheKey = `registry:data:${databaseFingerprint}`
+
+      let registry = await redisCache.get(dataCacheKey)
+
+      if (!registry) {
+        registry = await registryService.getReportRegistry()
+        await redisCache.set(dataCacheKey, JSON.stringify(registry))
+      }
+
+      reply.header('ETag', currentEtag)
       reply.send(registry)
     }
   )
